b17a8afbe275e43c467f0ecd7f349c9bfd91d5fb
safi-io <m.safi.ullah@outlook.com>
2024-07-17 21:01:39 +0500

    Creating Production grade React Components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
-import Header from "./header/header";
-import Footer from "./Footer/Footer"
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from './header/LogoutBtn'
 
-export {Header, Footer}
+
+export { Header, Footer, Container, Logo, LogoutBtn };
